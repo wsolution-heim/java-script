b6c03ad48dc423846e980ae90bbb3daadc647daa
Claudia <wsolution.heim@gmail.com>
2019-11-12 11:43:55 +0100

    basic variables

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10 / 3 *2 - 4;
+
+// let calculationDescription = '0 + 10 / 3 *2 - 4'; 
+
+let calculationDescription = `(${defaultResult} + 10) / 3 *2 - 4`; 
+
+outputResult(currentResult, calculationDescription);
+// outputResult(currentResult, '');
